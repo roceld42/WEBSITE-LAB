aa70ae28a04e14fbf5673815c97db4b627bb42cf
roceld42 <deleonrocel42@gmail.com>
2024-11-16 21:25:13 -0700

    Style: update project pictures feat: add for loop for ordered list

--- js/about.js
+++ js/about.js
@@ -10,6 +10,19 @@ button.addEventListener("mouseout", () => {
     document.getElementById("btn-alert").textContent="Hello!";
 })
 
+// Loop
+
+let list = document.getElementById("numbers");
+for (let i = 1; i <= 12; i = i + 1) {
+    let listItem = document.createElement("li");
+    if (i % 2 === 0) {
+        listItem.textContent = "even";
+    } else {
+        listItem.textContent = "odd";
+    }
+    list.appendChild(listItem);
+}
+
 // Footer 
 
 const d = new Date();
